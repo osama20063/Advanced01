ad3922c28221bd08f699c7f7e5f1e3355998e979
osama ahmed <oa9064626@gmail.com>
2026-08-28 23:29:55 +0300

    Q3

--- Program.cs
+++ Program.cs
@@ -28,39 +28,54 @@ namespace AssignmentAAdvanced_1
 
             #region Q2
 
-//            Q2: Write a generic class Container<T> with Add and Get methods.
+            //            Q2: Write a generic class Container<T> with Add and Get methods.
 
 
 
-//public class Container<T>
+            //public class Container<T>
+            //        {
+            //            private T[] _items;
+            //            private int _count;
+
+            //            public Container(int capacity = 10)
+            //            {
+            //                _items = new T[capacity];
+            //                _count = 0;
+            //            }
+
+            //            public void Add(T item)
+            //            {
+            //                if (_count < _items.Length)
+            //                {
+            //                    _items[_count] = item;
+            //                    _count++;
+            //                }
+            //            }
+
+            //            public T Get(int index)
+            //            {
+            //                if (index >= 0 && index < _count)
+            //                {
+            //                    return _items[index];
+            //                }
+            //                throw new IndexOutOfRangeException("Index is out of range.");
+            //            }
+            //        }
+            #endregion
+
+
+            #region Q3
+//public class Pair<TKey, TValue>
 //        {
-//            private T[] _items;
-//            private int _count;
+//            public TKey Key { get; set; }
+//            public TValue Value { get; set; }
 
-//            public Container(int capacity = 10)
+//            public Pair(TKey key, TValue value)
 //            {
-//                _items = new T[capacity];
-//                _count = 0;
+//                Key = key;
+//                Value = value;
 //            }
-
-//            public void Add(T item)
-//            {
-//                if (_count < _items.Length)
-//                {
-//                    _items[_count] = item;
-//                    _count++;
-//                }
-//            }
-
-//            public T Get(int index)
-//            {
-//                if (index >= 0 && index < _count)
-//                {
-//                    return _items[index];
-//                }
-//                throw new IndexOutOfRangeException("Index is out of range.");
-//            }
-//        }
+        //}
             #endregion
 
 
