f3c3f91832b4cc7d9c1030de1c671cc7e9cb422e
osama ahmed <oa9064626@gmail.com>
2026-08-28 23:26:02 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -5,7 +5,31 @@ namespace AssignmentAAdvanced_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            #region Q1
+            /*
+             * Q1: What is a generic class? Why use generics?
+             * 
+             * Answer:
+             * What is a Generic Class?
+             *   A generic class is a class that allows you to define placeholders 
+             *   for data types. The actual data type is specified when the class is instantiated.
+             * 
+             *  Why use Generics?
+             *   1. Type Safety: Catch type mismatch errors at compile-time rather than runtime.
+             *   2. Code Reusability: Write code once and use it with any data type.
+             *   3. Performance: Avoids boxing/unboxing overhead when working with value types 
+             */
+            #endregion
+
+
+
+
+
+
+
+
         }
     }
 }
